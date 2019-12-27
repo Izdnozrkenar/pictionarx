8f46ddf02ea33d5588a4c13b5d8708fe76724d4c
Franciszek Gorczyca <izdnozrkenar@github.com>
2019-12-26 16:46:11 -0800

    fixed recording to playing first time after pressing button

--- frontend/pictonarx_ui/src/voice_recorder/record-voice-button.js
+++ frontend/pictonarx_ui/src/voice_recorder/record-voice-button.js
@@ -2,63 +2,61 @@ import React from 'react'
 import './style.css'
 
 export default class RecordVoiceButton extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            stateRecorder: null,
-            audioChunks: []
-        }
-
-        this.clearAudio = this.clearAudio.bind(this);
-        this.toggleRecording = this.toggleRecording.bind(this);
+    state = {
+        isRecordingEnabled: false,
+        audioChunks: []
     }
+
 
     toggleRecording = () => {
-        this.setState({isRecordingEnabled: !this.state.isRecordingEnabled});
-        
-        if (!this.state.isRecordingEnabled) {
-            this.startRecording();
-        } else {
-            this.state.mediaRecorder.stop();
+        this.setState({ isRecordingEnabled: !this.state.isRecordingEnabled }, () => {
+            if (this.state.isRecordingEnabled) {
+                this.startRecording();
+            } else {
+                console.log(this.state.audioChunks)
 
-            this.playRecordedAudio();
-            
-            this.clearAudio();
-        };
-    }
-    
-    startRecording = () => {
-        navigator.mediaDevices.getUserMedia({audio: true})
-        .then(stream => {
-            
-            this.setState({mediaRecorder : new MediaRecorder(stream)})
-            
-            this.state.mediaRecorder.start();
-            
-            this.state.mediaRecorder.addEventListener("dataavailable", e => {
-                this.setState({audioChunks: this.state.audioChunks.concat(e.data)})
-            });
+                this.state.mediaRecorder.addEventListener("stop",() => {
+                    this.playRecordedAudio();
+                    this.clearAudio();
+                })
+
+                this.state.mediaRecorder.stop();
+            };
         });
+    }
+
+    startRecording = () => {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+
+                this.setState({ mediaRecorder: new MediaRecorder(stream) })
+
+                this.state.mediaRecorder.start();
+
+                this.state.mediaRecorder.ondataavailable = (e) => {
+                    console.log(e.data)
+                    this.setState({ audioChunks: this.state.audioChunks.concat(e.data) })
+                };
+            });
     }
 
     playRecordedAudio = () => {
         const audioBlob = new Blob(this.state.audioChunks);
         const audioUrl = URL.createObjectURL(audioBlob);
         const audio = new Audio(audioUrl);
-            
-        audio.play(); 
-    }
-    
-    clearAudio = () => {
-        this.setState({audioChunks: []})
+
+        audio.play();
     }
 
-    render(){
+    clearAudio = () => {
+        this.setState({ audioChunks: [] })
+    }
+
+    render() {
         return (
-            <button 
+            <button
                 onClick={this.toggleRecording}
                 className="recordButton" >
-            {this.state.isRecordingEnabled ? "Stop recording" : "Start recording"}</button>)
+                {this.state.isRecordingEnabled ? "Stop recording" : "Start recording"}</button>)
     }
 }
